2ab188011508953e79662e230c42691656cb5140
hpnightowl <iamhp2k@gmail.com>
2022-02-09 05:27:48 +0530

    sm8250-common: drop audio.a2dp.default
    - building from here
      https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35
    
    Change-Id: Ib71c1abd381468ec3621d60e5ca959f738f51168

--- common.mk
+++ common.mk
@@ -149,7 +149,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl \
     android.hardware.bluetooth.audio-impl \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.kona \
     audio.r_submix.default \
